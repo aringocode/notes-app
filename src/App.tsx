271dc8d35a90b6eff987a80131719723bf29ff69
akonshina <arina.konshina@teskalabs.com>
2025-01-25 08:48:50 +0100

    add func for notes and creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import { useMemo } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import { v4 as uuidV4 } from "uuid";
+
 import { NewNoteScreen } from "./NewNoteScreen.tsx";
+import { useLocalStorage } from "./useLocalStorage";
+
 
 export type Note = {
 	id: string
@@ -18,37 +24,65 @@ export type NoteData = {
 	tags: Tag[]
 }
 
-function App() {
+export type RawNote = {
+	id: string
+} & RawNoteData
+  
+export type RawNoteData = {
+	title: string
+	markdown: string
+	tagIds: string[]
+}
 
-  return (
-	<Container className="my-4">
-		<Routes>
-			<Route
-				path="/"
-				element={
-					<h1>Hi</h1>
-				}
-			/>
-			<Route
-				path="/new"
-				element={
-					<NewNoteScreen />
-				}
-			/>
-			<Route path="/:id" element={<h1>veve</h1>}>
-				<Route index element={<h1>lal</h1>} />
+function App() {
+	const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+	const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+
+
+	const notesWithTags = useMemo(() => {
+		return notes.map(note => {
+			return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id)) }
+		});
+	}, [notes, tags]);
+
+
+	function onCreateNote({ tags, ...data }: NoteData) {
+		setNotes(prevNotes => {
+		  return [
+			...prevNotes,
+			{ ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) },
+		  ]
+		})
+	};
+
+	return (
+		<Container className="my-4">
+			<Routes>
 				<Route
-					path="edit"
+					path="/"
 					element={
-						<h1>lsls</h1>
+						<h1>Hi</h1>
 					}
 				/>
-			</Route>
-			<Route path="*" element={<Navigate to="/" />} />
-		</Routes>
-	</Container>
-	
-  )
+				<Route
+					path="/new"
+					element={
+						<NewNoteScreen onSubmit={onCreateNote} />
+					}
+				/>
+				<Route path="/:id" element={<h1>veve</h1>}>
+					<Route index element={<h1>lal</h1>} />
+					<Route
+						path="edit"
+						element={
+							<h1>lsls</h1>
+						}
+					/>
+				</Route>
+				<Route path="*" element={<Navigate to="/" />} />
+			</Routes>
+		</Container>
+	)
 }
 
 export default App
